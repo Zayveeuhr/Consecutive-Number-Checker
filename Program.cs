ffa4f79f303eae46cee681c070e973560a1c73d3
daelynx <daelynxwinston@gmail.com>
2025-05-17 10:40:01 -0400

    Clean visual studio project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsNum
+{
+    class program
+    {
+        static void Main(string[] argS)
+        {
+            Console.WriteLine("Enter a series of numbers split by a hyphen '-': ");
+            var input = Console.ReadLine();
+
+            var inputSplit = input.Split('-');
+
+        }
+    }
+}
